dab95a545f8e4c51887ee94631b2c565e0a154bb
Makazmcfee1 <markarmstrong10.ma@gmail.com>
2023-01-22 21:03:32 +0000

    nav

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -20,7 +20,6 @@ import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
 import logo from '../assets/logo.png';
-import logo from '../assets/logo.svg';
 import mobile from '../assets/mobile.png';
 import mu5 from '../assets/mu5.png';
 import nb from '../assets/nb.png';
@@ -34,3 +33,42 @@ import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
+
+
+export default{
+    email,
+    mobile,
+    api,
+    cpp,
+    css,
+    figma,
+    flutter,
+    git,
+    graphql,
+    html,
+    javascript,
+    mu5,
+    node,
+    python,
+    react,
+    redux,
+    sass,
+    typescript,
+    vue,
+    about01,
+    about02,
+    about03,
+    about04,
+    profile,
+    circle,
+    logo,
+    adidas,
+    amazon,
+    asus,
+    bolt,
+    nb,
+    skype,
+    spotify,
+    bgIMG,
+    bgWhite,
+};
